2cac4726d65b2cbd28e8befb13c31282b6fc96ac
Varun Kumar <heysadboy@gmail.com>
2021-04-20 19:00:24 +0900

    updating app

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import { useState, React } from 'react';
 import CardList from './CardList';
 import hiraganaData from '../assets/data/hiragana.json';
 import katakanaData from '../assets/data/katakana.json';
 import '../assets/css/App.css';
 
-
 const App = () => {
     const hiragana = hiraganaData;
     const katakana = katakanaData;
+    const [kana, setKana] = useState(hiragana);
+
+    const toggleKana = (e) => {
+        if (e.target.innerText === "Hiragana") {
+            setKana(hiragana);
+        }
+        else if (e.target.innerText === "Katakana") {
+            setKana(katakana);
+        }
+
+    };
+
     return (
         <div className="ui container">
-            <CardList kanaList={hiragana} />
+            <div className="ui grid">
+                <div className="ten wide column">
+                    <h1 className="ui header">
+                        <i className="pencil alternate icon"></i>
+                        <div className="content">Kana Practice</div>
+                    </h1>
+                </div>
+                <div className="six wide column">
+                    <button className="ui blue button" onClick={toggleKana}>Hiragana</button>
+                    <button className="ui blue button" onClick={toggleKana}>Katakana</button>
+                </div>
+            </div>
+            <CardList kanaList={kana} />
         </div>
     );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import '../assets/css/CharacterCard.css';
 
-const CharacterCard = ({kana}) => {
+const CharacterCard = ({ kana }) => {
+    const [isCorrect, setIsCorrect] = useState(undefined)
+
+    const checkKana = (e) => {
+        if (e.target.value === "") {
+            setIsCorrect("");
+        }
+        else if (e.target.value === kana.en) {
+            setIsCorrect("green");
+        }
+        else {
+            setIsCorrect("red");
+        }
+    };
+
     return (
-        <div className="ui card">
+        <div className={`ui ${isCorrect} card`}>
             <div className="content">
                 <div className="character">{kana.jp}</div>
             </div>
             <div className="extra content">
                 <div className="ui huge transparent input">
-                    <input type="text" />
+                    <input type="text" maxLength="3" onChange={checkKana} />
                 </div>
             </div>
         </div>
